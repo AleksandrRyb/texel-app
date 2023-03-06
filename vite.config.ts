efeb7210b86aba57933dc8acc7d3dfd461cf551e
AleksandrRyb <inkognito19931@gmail.com>
2023-03-06 14:31:17 +0300

    коректно настроил пути

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +7,8 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
+  },
+  resolve: {
+    alias: [{ find: '@hoc', replacement: 'src/hoc' }],
   },
 });
