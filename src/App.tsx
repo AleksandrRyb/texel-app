0da7002bb5d31c60fd80a9f61e65206f3389f9c9
AleksandrRyb <inkognito19931@gmail.com>
2023-03-07 11:28:51 +0300

    connect store to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { ThemeProvider } from '@hoc/theme-provider';
+import { Provider } from 'react-redux';
+import { setupStore } from 'store/store';
 import './App.css';
+
+const store = setupStore();
 
 const App: React.FC = () => {
   return (
-    <ThemeProvider>
-      <div>Hello World</div>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <div>Hello World</div>
+      </ThemeProvider>
+    </Provider>
   );
 };
 
