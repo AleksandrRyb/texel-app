ef8f1d10ec979503dd59589882cfbd199e6ebba7
AleksandrRyb <inkognito19931@gmail.com>
2023-03-07 16:29:13 +0300

    Have added first action creator

---
+++ src/store/scheme/scheme.actions-creators.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { IScheme } from '@models/scheme.model';
+
+export const getConfig = createAsyncThunk('scheme/getScheme', async (_, thunkApi) => {
+  try {
+    const response = await axios.get<IScheme>('http://localhost:4000/config');
+    return response.data;
+  } catch (error) {
+    return thunkApi.rejectWithValue('some error');
+  }
+});
