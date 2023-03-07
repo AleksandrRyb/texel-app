0cf781ff24fa910acf6fb0e124e8db0f1f8ebe0c
AleksandrRyb <inkognito19931@gmail.com>
2023-03-07 11:25:49 +0300

    have added custom hooks for redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
 import { ThemeProvider } from '@hoc/theme-provider';
+import './App.css';
 
 const App: React.FC = () => {
   return (
