ffe00ee9c60b15dece973698ede54ed1ee0b630d
AleksandrRyb <inkognito19931@gmail.com>
2023-03-09 12:40:44 +0300

    Подключи redux-logger

--- src/pages/form-page/form-page.tsx
+++ src/pages/form-page/form-page.tsx
@@ -1,12 +1,8 @@
-import { FC, useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '@hooks/redux';
-import { getConfigAction } from '@store/scheme/scheme.actions-creators';
+import { FC } from 'react';
 import { schemeApi } from '@services/scheme.api';
 
 const FormPage: FC = () => {
   const { data: config, error, isLoading } = schemeApi.useGetConfigQuery({});
-
-  console.log(isLoading);
 
   return <div style={{ color: 'red' }}>{JSON.stringify(config)}</div>;
 };
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { createLogger } from 'redux-logger';
 import { schemeApi } from '@services/scheme.api';
+
+const logger = createLogger({
+  collapsed: true,
+});
 
 const rootReducer = combineReducers({
   [schemeApi.reducerPath]: schemeApi.reducer,
@@ -8,7 +13,8 @@ const rootReducer = combineReducers({
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(schemeApi.middleware),
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(logger, schemeApi.middleware),
   });
 };
 
