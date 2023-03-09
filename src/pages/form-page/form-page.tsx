8617c9432c82ca1ed09c483a5ba9e41975a1df19
AleksandrRyb <inkognito19931@gmail.com>
2023-03-09 17:54:22 +0300

    Add header

--- src/pages/form-page/form-page.tsx
+++ src/pages/form-page/form-page.tsx
@@ -1,10 +1,23 @@
 import { FC } from 'react';
+import { Title, Container, Text, Flex } from '@mantine/core';
 import { schemeApi } from '@services/scheme.api';
 
 const FormPage: FC = () => {
   const { data: config, error, isLoading } = schemeApi.useGetConfigQuery({});
 
-  return <div style={{ color: 'red' }}>{JSON.stringify(config)}</div>;
+  return (
+    <Container>
+      <Flex>
+        <Title mb={10} order={1}>
+          {config?.name}
+        </Title>
+        <Title mb={15} order={3}>
+          {config?.title}
+        </Title>
+        <Text fz="md">{config?.description}</Text>
+      </Flex>
+    </Container>
+  );
 };
 
 export default FormPage;
