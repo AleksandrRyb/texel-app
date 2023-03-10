9733d796b25021468ee68ccb303abafe2508cea6
AleksandrRyb <inkognito19931@gmail.com>
2023-03-10 10:22:15 +0300

    Lightly styles corrections

--- src/components/dinamic-input/dinamic-input.tsx
+++ src/components/dinamic-input/dinamic-input.tsx
@@ -1,4 +1,5 @@
 import { InputTypes } from '@constants/input-constants';
+import styled from '@emotion/styled';
 import { NumberInput, Select } from '@mantine/core';
 import { InputItem, InputParametr } from '@models/scheme.model';
 import React from 'react';
@@ -7,12 +8,23 @@ const DinamicInput: React.FC<{ input: InputParametr }> = ({ input }) => {
   return (
     <>
       {input.type === InputTypes.Number ? (
-        <NumberInput label={input.title} placeholder={input.title} />
+        <CustomNumberInput min={0} mb="15px" label={input.title} placeholder={input.title} />
       ) : (
-        <Select label={input.title} placeholder={input.title} data={input?.items as InputItem[]} />
+        <Select
+          mb="15px"
+          label={input.title}
+          placeholder={input.title}
+          data={input?.items as InputItem[]}
+        />
       )}
     </>
   );
 };
+
+const CustomNumberInput = styled(NumberInput)`
+  & .mantine-NumberInput-control {
+    border-radius: 0px;
+  }
+`;
 
 export default DinamicInput;
--- src/pages/form-page/form-page.tsx
+++ src/pages/form-page/form-page.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Title, Container, Text, Box } from '@mantine/core';
+import styled from '@emotion/styled';
+import { Title, Container, Text, Flex } from '@mantine/core';
 import { useForm } from 'react-hook-form';
 import { schemeApi } from '@services/scheme.api';
 
@@ -16,23 +17,31 @@ const FormPage: React.FC = () => {
 
   return (
     <Container>
-      <Title mb={10} order={1}>
+      <Title mb="10px" order={1}>
         {config?.name}
       </Title>
-      <Title mb={15} order={3}>
+      <Title mb="15px" order={3}>
         {config?.title}
       </Title>
-      <Text fz="md">{config?.description}</Text>
+      <Text mb="30px" fz="md">
+        {config?.description}
+      </Text>
 
-      <Box>
-        <form>
+      <CustomFlex align="center" justify="center">
+        <form style={{ width: '60%' }}>
           {config?.parameters.input.map((input) => (
             <DinamicInput key={input.title} input={input} />
           ))}
         </form>
-      </Box>
+      </CustomFlex>
     </Container>
   );
 };
+
+const CustomFlex = styled(Flex)`
+  height: 300px;
+  border: 1px solid white;
+  border-radius: 1%;
+`;
 
 export default FormPage;
